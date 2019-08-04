5d9396b377c6695968ecb2517b003aa6c2b5aa6d
wudi <609679329@qq.com>
2019-08-04 22:04:49 +0800

    update

--- design-pattern/src/单例模式/SinglePatternHungry.java
+++ design-pattern/src/单例模式/SinglePatternHungry.java
@@ -5,19 +5,25 @@ import java.lang.reflect.Field;
 /**
  * 单例模式（饿汉式）
  * @since 2019/8/4
- * 饿汉式单例模式需要注意两个点
- * 1.static 保证了不是new的情况也能调用
  */
 public class SinglePatternHungry {
-
-    private static final SinglePatternHungry sph = new SinglePatternHungry();
+    /**
+     * 1.懒汉式不存在线程安全问题
+     * 2.由于要被静态方法调用所以必须是静态属性
+     * 3.final也保证了不可修改性
+     *   需要注意的是懒汉式不能用final修改，
+     *   因为final修改的属性要求必须在类加载完成时候初始化完成
+     *   而不是在调用的时候初始化完成
+     */
+    private static final SinglePatternHungry singlePatternHungry = new SinglePatternHungry();
 
     private SinglePatternHungry() {}
 
     public static SinglePatternHungry getInstance() {
-        return sph;
+        return singlePatternHungry;
     }
 
+    //这里发现通过反射还是可以修改掉这个对象的值的
     public static void main(String[] args) throws Exception {
         Field declaredField = SinglePatternHungry.getInstance().getClass().getDeclaredField("sph");
         declaredField.setAccessible(true);
--- design-pattern/src/单例模式/SinglePatternLazy.java
+++ design-pattern/src/单例模式/SinglePatternLazy.java
@@ -1,4 +1,85 @@
 package 单例模式;
 
+import java.lang.reflect.Field;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 单例模式（懒汉式）
+ *
+ * @since 2019/8/4
+ */
 public class SinglePatternLazy {
+
+    static int i = 0;
+
+    private static SinglePatternLazy singlePatternLazy;
+
+    public static CyclicBarrier cyclicBarrier = new CyclicBarrier(10, () -> {
+        Field singlePatternLazy;
+
+        try {
+            singlePatternLazy = SinglePatternLazy.getInstance().getClass().getDeclaredField("singlePatternLazy");
+            singlePatternLazy.setAccessible(true);
+            singlePatternLazy.set("singlePatternLazy", null);
+            System.out.println(Thread.currentThread().getName() + " set zero");
+
+            if (i < 2) i = 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    });
+
+
+
+    private SinglePatternLazy() {
+    }
+
+    public static SinglePatternLazy getInstance() {
+        if (singlePatternLazy == null) {
+            synchronized (SinglePatternLazy.class) {
+                if (singlePatternLazy == null) {
+                    singlePatternLazy = new SinglePatternLazy();
+                    i++;
+                    System.out.println(Thread.currentThread().getName() + " true");
+                } else {
+                    System.out.println(Thread.currentThread().getName() + "2");
+                }
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + " false");
+        }
+        return singlePatternLazy;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int count = 0;
+        while (true) {
+            count++;
+
+            for (int j = 0; j < 10; j++) {
+                Thread thread = new Thread(() -> {
+                    getInstance();
+                    try {
+                        cyclicBarrier.await();
+                    } catch (InterruptedException | BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                });
+                thread.setName("count" + count + ", " + j);
+                thread.start();
+            }
+
+            if (i >= 2) {
+                break;
+            }
+            System.out.println(count + "次");
+            while (i != 0) {
+                Thread.sleep(10L);
+            }
+        }
+
+    }
+
+
 }
